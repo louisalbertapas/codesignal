41cee4d9b5a1a4ea05ce5d4115bad9ddfedd4209
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-11-15 14:52:18 +0800

    Fix indentation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-using codesignal.LabyrinthOfNestedLoops;
+using codesignal.Arcade.Core.LabyrinthOfNestedLoops;
 
 void main()
 {
-  Console.WriteLine("Select from the following methods above on which to run.");
-  var result = IsPower.solution(125);
-  Console.WriteLine(result);
+    Console.WriteLine("Select from the following methods above on which to run.");
+    var result = IsPower.solution(125);
+    Console.WriteLine(result);
 }
 
 main();
