e77d1f326d1a33416e2072a8a103777df31d8a31
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-11-15 11:47:22 +0800

    Add solution for "Is Power?" problem

---
+++ LabyrinthOfNestedLoops/IsPower.cs
@@ -0,0 +1,23 @@
+namespace codesignal.LabyrinthOfNestedLoops
+{
+  public static class IsPower
+  {
+    public static bool solution(int n)
+    {
+      if (n == 1) return true;
+      for (int i = 2; i <= 20; ++i)
+      {
+        int b = i * i;
+        while (b <= n)
+        {
+          if (b == n)
+            return true;
+
+          b *= i;
+        }
+      }
+
+      return false;
+    }
+  }
+}
